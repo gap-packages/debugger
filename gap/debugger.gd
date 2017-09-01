66c1d6ad1a0f598d690c4f3f8c3b478dcb53ff5b
Chris Jefferson <caj21@st-andrews.ac.uk>
2017-09-01 09:04:30 +0200

    Improve documentation

--- gap/debugger.gd
+++ gap/debugger.gd
@@ -8,13 +8,13 @@
 #! @Description
 #!   Adds a breakpoint to all loaded files whose name
 #!   ends <A>file</A>, at line <A>line</A>.
-#!   Optionally a <A>function</A> to call can be passed
-#!   in, or by default the break loop will be entered.
+#!   Optionally a <A>function</A> to call can be given.
+#!   The default function enters the break loop.
 DeclareGlobalFunction( "AddBreakpoint" );
 
 #! @Arguments file, line
 #! @Description
-#!   Remove a breakpoints in loaded files whose name
+#!   Remove all breakpoints in loaded files whose name
 #!   ends <A>file</A>, at line <A>line</A>.
 DeclareGlobalFunction( "ClearBreakpoint" );
 
