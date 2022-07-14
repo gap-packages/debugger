0279a9eb8f5681c0fdf8be2478fd7a17f965f818
Max Horn <max@quendi.de>
2022-07-14 14:35:55 +0200

    Don't fuzz around with gmp.h

--- src/debugger.cc
+++ src/debugger.cc
@@ -2,12 +2,6 @@
  * debugger: Debugging support for GAP
  */
 
-// Include gmp.h *before* switching to C mode, because GMP detects when
-// compiled from C++ and then does some things differently, which would
-// cause an error if called from within extern "C". But libsing.h
-// (indirectly) includes gmp.h ...
-#include <gmp.h>
-
 extern "C" {
 #include "src/compiled.h"          /* GAP headers                */
 #include "src/hookintrprtr.h"
--- src/gap_cpp_headers/include_gap_headers.hpp
+++ src/gap_cpp_headers/include_gap_headers.hpp
@@ -1,9 +1,6 @@
 #ifndef _GAP_HEADERS_H_QEHJ
 #define _GAP_HEADERS_H_QEHJ
 
-#include <gmp.h>
-extern "C" {
 #include "src/compiled.h"
-}
 
 #endif
